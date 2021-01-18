44cf6112abfead702e840b1caf746ede79b66ea6
Denis Pasquier <chenilleppp@gmail.com>
2021-01-18 17:37:06 +0100

    Initial commit

---
+++ MesAllias.sh
@@ -0,0 +1,17 @@
+# Avec la commande "clear":
+# Efface le contenu de la console
+alias cls="clear"
+cls
+# Efface le contenu de la console # Avec la commande Linux "clear" ou avec la command DOS 'cls'
+# Pour activer 'cls'
+# 'alias cls="clear"'
+# Maintenant, pour efface le contenu de la console
+# tapé soit 'clear' (linux) ou 'cls' (DOS)
+
+# Avec la commande "ls":
+# Affiche la liste des fichier et répertoir en coleurs par type d'objets
+# Pour activer
+alias ls="ls --color=tty"
+
+ls # Pour vérifier...
+
