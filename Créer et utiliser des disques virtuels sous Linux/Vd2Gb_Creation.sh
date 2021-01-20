651d99f761c0c8befe8706dec965b3608da1abdf
AgroSmart <37621067+AgroSmartSuisse@users.noreply.github.com>
2021-01-20 16:07:45 +0100

    Update pade_20210120.html

--- Créer et utiliser des disques virtuels sous Linux/Vd2Gb_Creation.sh
+++ Créer et utiliser des disques virtuels sous Linux/Vd2Gb_Creation.sh
@@ -1,3 +1,17 @@
-echo Salut denis
+#
+# Copyright (c) Denis Pasquier 2021 -  1 -  20
+#
+
+
+declare -g maintenant
+maintenant = date +%m/%d/%y
+print maintenant
+
+
+
+
+
+
+
 
 
---
+++ EnVraques/Eteindre l'écran.sh
@@ -0,0 +1,11 @@
+#
+# Copyright (c) Denis Pasquier 2021 -  1 -  21
+#
+
+# Eteindre l'écran.sh
+#
+#
+# Sur les ordinateurs portable, les écrans n'ont parfois pas de bouton pour éteindre l'écran, ou le mettre en veille.
+#
+# Sur Linux, une commande existe pour éteindre l'écran. Il se rallumera après un mouvement de la souris:
+sleep 1; xset dpms force off
