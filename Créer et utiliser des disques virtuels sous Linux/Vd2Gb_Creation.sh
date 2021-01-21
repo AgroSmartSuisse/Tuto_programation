cff820c2f406d2affa7e5621fde909ae82571560
Denis Pasquier <chenilleppp@gmail.com>
2021-01-21 17:09:36 +0100

    local

--- Créer et utiliser des disques virtuels sous Linux/Vd2Gb_Creation.sh
+++ Créer et utiliser des disques virtuels sous Linux/Vd2Gb_Creation.sh
@@ -6,17 +6,10 @@
 
 #maintenant = date(+%m/%d/%y) ; echo ${#maintenant}
 
-
+# construction et affichage du fichier texte d'acceuil tmp.txt
+nf="tmp.txt" # nom du fichier info
 #date --date="+%m/%d/%y"
-date "+%m %d %H %M %Y.%S" > tmp
-print "denis pasquier" >> tmp
-
-
-
-
-
-
-
-
-
-
+date "+%m %d %H %M %Y.%S" > ${nf}
+echo Bonjour Denis Pasquier | sed -e "s/Denis Pasquier/pade/">> ${nf}
+echo "Denis Pasquier" >> ${nf}
+cat ${nf}
