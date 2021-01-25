489ec086d4cd3fea97c99aea447057b69899180f
Denis Pasquier <chenilleppp@gmail.com>
2021-01-25 14:16:07 +0100

    pade

---
+++ EnVraques/Afficher_une_date.sh
@@ -0,0 +1,13 @@
+#
+# Copyright (c) Denis Pasquier 2021 -  1 -  22
+#
+
+# shellcheck disable=SC2094
+echo -n "denis ppp " & date  "+%d-%m-%Y %Hheur %Mminutes"  > vadardat
+#read nom "denis pasquier"
+
+cat vadardat
+#text = $vadardat" "$nom
+#echo $text
+
+
--- EnVraques/pile-ou-face_BASH.sh
+++ EnVraques/pile-ou-face_BASH.sh
@@ -1,25 +1,22 @@
-## Copyright (c) Denis Pasquier 2021 -  1 -  22
+#Copyright (c) Denis Pasquier 2021 -  1 -  22
 #
-# Pile ou face en BASH
-
-#  tirage$ = shuf -i 0-1 -n 1 | sed -e 's/1/pile/' -e 's/0/face/'
+#
+#Pile ou face en BASH
+#  shuf -i 0-1 -n 1 | sed -e 's/1/pile/' -e 's/0/face/'
 #
 
+nf="tmp.txt" # nom du fichier de sortie
+
+echo -n "Tirages pile ou face du " >${nf}
+date "+%A %d %B %Y à %H heurs %M minutes %S secondes" >> ${nf}
+echo ----------------------------------------------------- >> ${nf}
 
 for i in `seq 1 25`
   do
-    ms ="Tirage $i"
-    # shellcheck disable=SC2037
-    ms2 =huf -i 0-1 -n 1 | sed -e 's/1/pile/' -e 's/0/face/'
 
-    echo ${ms}${ms2}
+      echo -n "Tirage  $i " >> ${nf}
+      echo -n | shuf -i 0-1 -n 1 | sed -e 's/1/pile/' -e 's/0/face/' >> ${nf}
 
   done
-#for i in `seq 1 4`
-#do
-#
-#  "Tirage $i"
-#  dede2 = shuf -i 0-1 -n 1
-#  dede3 = sed -e 's/1/pile/' -e 's/0/face/'
-#
-#done
+
+  cat ${nf}
