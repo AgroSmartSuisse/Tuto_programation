746a5193c7873a07a7d1e6550f6fed0ae63954de
Denis Pasquier <chenilleppp@gmail.com>
2021-01-27 16:28:54 +0100

    de locale master

--- Créer et utiliser des disques virtuels sous Linux/Vd2Gb_Creation.sh
+++ Créer et utiliser des disques virtuels sous Linux/Vd2Gb_Creation.sh
@@ -6,13 +6,21 @@
 
 
 #date --date="+%m/%d/%y"
-
-# nf="tmp.txt" # nom du fichier info
+# acceuille ---------------------------------------------------------------------------
+echo aceuille ---------------------------------------------------------------------------
 qui="Denis Pasquier"
-
-
+# FormaDate="""""+Nous sommes le %A %d %B %Y, il est %H heurs %M minutes et %S secondes""""""
 date "+Nous sommes le %A %d %B %Y, il est %H heurs %M minutes et %S secondes"
 echo ${qui} 'Vous souhaite la bien venue'
 echo Création d"'"un disque virtuel, marche à suivre
 echo Source video  de la chaine youtube '"'Adrien Linuxtricks'"' 'https://www.youtube.com/watch?v=GH_6pMvmKe0'
-# cat ${nf}
+echo acceuille Fin---------------------------------------------------------------------------
+# acceuille Fin---------------------------------------------------------------------------
+
+echo '##### ls -alF --color=auto /dev/'
+echo '##### ls -alF --color=auto /dev/sd*'
+echo 'sudo -i'
+echo 'cd /'
+echo 'mkdir /vdisks'
+echo 'dd if=/dev/zero of=/vdisks/vdisk2G01 bs=1M count=2000'
+
