03be3bdfd3569edb71bdbc760afd7d24a34f0072
Denis Pasquier <chenilleppp@gmail.com>
2021-01-27 16:28:54 +0100

    de locale master

--- EnVraques/Afficher_une_date.sh
+++ EnVraques/Afficher_une_date.sh
@@ -2,12 +2,7 @@
 # Copyright (c) Denis Pasquier 2021 -  1 -  22
 #
 
-# shellcheck disable=SC2094
-echo -n "Denis vous salut! " & date  "+Nous sommes le %A %d %B de l'ans %Y. Il est %H heures %M minutes et %S secondes"  # > vadardat
-#read nom "denis pasquier"
-
-# cat vadardat
-#text = $vadardat" "$nom
-#echo $text
+MonNom="Denis"
+echo -n ${MonNom} "vous salut! " & date  "+Nous sommes le %A %d %B de l'ans %Y. Il est %H heures %M minutes et %S secondes"
 
 
